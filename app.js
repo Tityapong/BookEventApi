b0160cd4997a16363d3852044b4e12a2923d86fc
Your Name <you@example.com>
2024-12-14 11:28:20 +0700

    change

--- app.js
+++ app.js
@@ -4,4 +4,3 @@ const bcrypt = require('bcrypt');
 bcrypt.hash('pongpong2233', 10).then((hashedPassword) => {
   console.log(hashedPassword);  // Copy this hashed value and update the password in your database
 });
-mysql://root:MhJpnXnprawDYwlGDguzLkEurDFqZhGZ@junction.proxy.rlwy.net:37895/railway
