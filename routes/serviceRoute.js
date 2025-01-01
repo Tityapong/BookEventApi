257ee585fd267586a559e58caed73e13eb08e380
Your Name <you@example.com>
2025-01-02 00:57:24 +0700

    add

--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -4,7 +4,7 @@ const { authorize } = require('../middleware/authorize'); // Import role-based m
 const { createServiceCategory , getAllServiceCategories , updateServiceCategory , deleteServiceCategory } = require('../controllers/serviceController');
 
 
-router.get('/admin/service-categories', authorize(['admin']), getAllServiceCategories);
+router.get('/admin/service-categories',  getAllServiceCategories);
 // POST route: Create a service category (Admin only)
 router.post('/admin/service-categories', authorize(['admin']), createServiceCategory);
 
